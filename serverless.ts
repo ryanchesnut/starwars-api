48be837dfc836b1e864478973bfbf5ad28b27225
ryanchesnut <37148876+ryanchesnut@users.noreply.github.com>
2023-03-19 23:42:03 -0400

    added swagger

--- serverless.ts
+++ serverless.ts
@@ -1,23 +1,28 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import skywalkerStarships from '@functions/skywalker-starships';
-import speciesClassificationEp1 from '@functions/species-classification'
-import galaxyPopulation from '@functions/galaxy-population'
+import skywalkerStarships from "@functions/skywalker-starships";
+import speciesClassificationEp1 from "@functions/species-classification";
+import galaxyPopulation from "@functions/galaxy-population";
 
 const serverlessConfiguration: AWS = {
-  service: 'starwars-api',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild', 'serverless-offline', 'serverless-dotenv-plugin'],
+  service: "starwars-api",
+  frameworkVersion: "3",
+  plugins: [
+    "serverless-esbuild",
+    "serverless-offline",
+    "serverless-dotenv-plugin",
+    "serverless-openapi-documentation",
+  ],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
   },
   // import the function via paths
@@ -28,11 +33,18 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
+    },
+    documentation: {
+      // Add the plugin configuration here
+      version: "1.0.0",
+      title: "My Service API",
+      description: "API for My Service",
+      endpoint: "https://my-service-api.example.com",
     },
   },
 };
--- src/functions/skywalker-starships/handler.ts
+++ src/functions/skywalker-starships/handler.ts
@@ -1,13 +1,20 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { formatJSONResponse } from '@libs/api-gateway';
-import { middyfy } from '@libs/lambda';
-import {getLukesStarships} from './controller'
+import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
+import { formatJSONResponse } from "@libs/api-gateway";
+import { middyfy } from "@libs/lambda";
+import { getLukesStarships } from "./controller";
 import { Starship } from "src/types";
 
-import schema from './schema';
-
+import schema from "./schema";
+/**
+ * Get a list of starships from the Star Wars universe.
+ *
+ * @route GET /starships
+ * @group Starships - Operations related to starships
+ * @returns {object} 200 - An array of starships
+ * @returns {Error}  default - Unexpected error
+ */
 const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
-  let data = await getLukesStarships() as Array<Starship>
+  let data = (await getLukesStarships()) as Array<Starship>;
   return formatJSONResponse({
     message: `SkywalkerStartships`,
     data,
